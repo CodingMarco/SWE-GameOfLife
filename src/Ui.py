baf3838af834ef1fb0131b1a8f8db8ec5c4cc1a2
CodingMarco <codingmarco@gmail.com>
2020-10-31 13:47:20 +0100

    Event loop

--- src/Ui.py
+++ src/Ui.py
@@ -7,14 +7,19 @@ class Ui:
     def __init__(self, window: pygame.display, cell_size):
         self.cell_size = cell_size
         self.window = window
+        self.living_color = pygame.Color(255, 255, 255)
+        self.dead_color = pygame.Color(0, 0, 0)
+        self.grid_color = pygame.Color(128, 128, 128)
+        self.grid_line_width = 2
 
     def draw_game_goard(self, board: Board):
+        self.window.fill(self.grid_color)
         for x in range(0, board.get_width()):
             for y in range(0, board.get_height()):
                 pygame.draw.rect(self.window,
-                                 pygame.Color(255, 255, 255) if board.get_living(x, y) else pygame.Color(0, 0, 0),
+                                 self.living_color if board.get_living(x, y) else self.dead_color,
                                  pygame.Rect(x * self.cell_size,
                                              y * self.cell_size,
-                                             self.cell_size-2,
-                                             self.cell_size-2))
+                                             self.cell_size-self.grid_line_width,
+                                             self.cell_size-self.grid_line_width))
 
--- src/main.py
+++ src/main.py
@@ -9,16 +9,19 @@ from Board import Board
 def main(board_width, board_height, cell_size):
     pygame.init()
     window = pygame.display.set_mode((board_width * cell_size, board_height * cell_size))
-    window.fill((128, 128, 128))
 
     board = Board(board_width, board_height)
     board.toggle_living(2, 2)
     board.toggle_living(3, 3)
     ui = Ui(window, cell_size)
-    ui.draw_game_goard(board)
 
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        ui.draw_game_goard(board)
+        pygame.display.update()
 
 
 if __name__ == '__main__':

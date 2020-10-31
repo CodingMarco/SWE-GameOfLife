342a42b9738c631de5064e98dd869e513b28bc0b
CodingMarco <codingmarco@gmail.com>
2020-10-31 13:08:55 +0100

    Width, height and cell_size parameters for main method

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@
 import pygame
 
 
-def main():
+def main(board_width, board_height, cell_size):
     pygame.init()
-    screen = pygame.display.set_mode((512, 512))
+    screen = pygame.display.set_mode((board_width * cell_size, board_height * cell_size))
     screen.fill((100, 255, 100))
     while pygame.event.wait().type != pygame.QUIT:
         pass
 
 
 if __name__ == '__main__':
-    main()
+    main(10, 10, 10)
 

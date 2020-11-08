046e72f9422f55e078060088b5bac2a10820ef6e
CodingMarco <codingmarco@gmail.com>
2020-11-08 16:50:43 +0100

    Modified controler to apply the next generation every n ms

--- src/controller.py
+++ src/controller.py
@@ -1,16 +1,20 @@
 import pygame
 from ui import Ui
 from board import Board
+from gamerules import GameRules
 
 
 class Controller:
+    GENERATIONUPDATE = pygame.USEREVENT+1
+
     def __init__(self, board_width, board_height, cell_size):
         self.board = Board(board_width, board_height)
         self.ui = Ui(cell_size, self.board)
+        self.new_generation_delay = 500
         self.quit = False
 
-
     def run(self):
+        pygame.time.set_timer(Controller.GENERATIONUPDATE, self.new_generation_delay)
         while not self.quit:
             self.update_ui()
             events = self.wait_for_at_least_one_event()
@@ -30,6 +34,8 @@ class Controller:
             self.quit = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             self.process_mouse_event()
+        elif event.type == Controller.GENERATIONUPDATE:
+            self.board = GameRules.apply_new_generation(self.board)
 
     def process_mouse_event(self):
         x, y = self.ui.mouse_coordinates_to_cell()

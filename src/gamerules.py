4435bcdf65e2d75adc4e0eb2d4a36ceec7e1e76f
SirBerni <b.sorge@t-online.de>
2020-11-08 17:20:09 +0100

    added gameruels

--- src/gamerules.py
+++ src/gamerules.py
@@ -3,5 +3,19 @@ class GameRules:
         pass
 
     def apply_new_generation(self, board):
-        # apply rules
-        return board
+        copy_board = board
+        for i in range(board.width):
+            for k in range(board.height):
+                nr_of_living = board.get_number_of_living_neighbours(i, k)
+                living = board. get_living(i, k)
+
+                if living == 0:      # check if field is living
+                    if nr_of_living == 3:  # if dead and 3 living => turn alive
+                        copy_board.toggle_living(i, k)
+
+                else:           # check if field is dead
+                    if nr_of_living == 1:  # if alive and less or equal 1 living => turn dead
+                        copy_board.toggle_living(i, k)
+                    elif nr_of_living >= 4:  # if alive and more or equal 4 living => turn dead
+                        copy_board.toggle_living(i, k)
+        return copy_board

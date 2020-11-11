01a3dc2d9cf3e269fed3af8a6f97052cd6e33896
CodingMarco <codingmarco@gmail.com>
2020-11-11 19:03:35 +0100

    Performance optimizations

--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame.color
 from board import Board
+import copy
 
 
 class Ui:
@@ -9,22 +10,24 @@ class Ui:
         self.living_color = pygame.Color(255, 255, 255)
         self.dead_color = pygame.Color(0, 0, 0)
         self.grid_color = pygame.Color(128, 128, 128)
-        self.grid_line_width = 2
+        self.grid_line_width = 1
 
         self.window = pygame.display.set_mode((board.get_width() * cell_size + self.grid_line_width,
                                                board.get_height() * cell_size + self.grid_line_width))
         self.window.fill(self.grid_color)
+        self.prev_board = copy.deepcopy(board)
 
     def draw_game_board(self, board: Board):
-        self.window.fill(self.grid_color)
         for x in range(0, board.get_width()):
             for y in range(0, board.get_height()):
-                pygame.draw.rect(self.window,
-                                 self.living_color if board.get_living(x, y) else self.dead_color,
-                                 pygame.Rect(x * self.cell_size + self.grid_line_width,
-                                             y * self.cell_size + self.grid_line_width,
-                                             self.cell_size-self.grid_line_width,
-                                             self.cell_size-self.grid_line_width))
+                if board.get_living(x, y) != self.prev_board.get_living(x, y):
+                    pygame.draw.rect(self.window,
+                                     self.living_color if board.get_living(x, y) else self.dead_color,
+                                     pygame.Rect(x * self.cell_size + self.grid_line_width,
+                                                 y * self.cell_size + self.grid_line_width,
+                                                 self.cell_size-self.grid_line_width,
+                                                 self.cell_size-self.grid_line_width))
+        self.prev_board = copy.deepcopy(board)
 
     def mouse_coordinates_to_cell(self):
         x, y = pygame.mouse.get_pos()
